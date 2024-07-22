4bd90f0feb56ef5ec7a89d373c0ded7462a08578
Makarios44 <mmakarios44@gmail.com>
2024-07-22 12:16:19 -0300

    mudança de nome da pasta e retirando configuração previa do static

--- soundtracks/settings.py
+++ soundtracks/settings.py
@@ -118,11 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "base_static",
-   
-]
-STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

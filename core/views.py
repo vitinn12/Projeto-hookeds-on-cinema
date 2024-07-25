6d230a0ae565c6ff96f733a61149f19b80aa7383
vitu <vitoralves1212.va@gmail.com>
2024-07-24 22:05:43 -0300

    atualizado os arquivos urls.py e views.py"

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login (request):
+    return render (request, 'paginas/login.html')
+
+def register (request):
+    return render (request, 'paginas/registro.html')
+
+def user (request):
+    return render (request, 'paginas/perfil_usuario.html')
+
+def index (request):
+    return render (request, 'paginas/index.html')
--- soundtracks/settings.py
+++ soundtracks/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'#nome do app
+    'core', #nome do app
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'soundtracks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

bc2c320085553d2fb8d1ee01d3d5da4f18a17ea1
Makarios44 <mmakarios44@gmail.com>
2024-07-26 10:08:34 -0300

    apenas atualizando a url da pagina principal

--- soundtracks/urls.py
+++ soundtracks/urls.py
@@ -5,7 +5,7 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('index/',views.index, name='paginaPrincipal'),
+    path ('',views.index, name='paginaPrincipal'),
     path ('login/', views.login, name='login'),
     path ('register/', views.register, name= 'registro'),
     path ('user/', views.user, name= 'areadousuario'),    

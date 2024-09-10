c98fcbdbbe85460047239e5b56c63548d19741bf
vitinn12 <vitoralves1212.va@gmail.com>
2024-09-10 17:48:53 -0300

    novos ajustes no arquivo index.html e index.css

---
+++ core/static/js/main.js
@@ -0,0 +1,16 @@
+window.revelar = ScrollReveal ({reset: true})
+
+revelar.reveal('.imagem-comp1',{
+    duration:1000,
+    origin:'right',
+    distance: '100px',
+    delay: 500,
+    reset: false
+}); 
+revelar.reveal('.texto-comp1',{
+    duration:1000,
+    origin:'right',
+    distance: '200px',
+    delay: 1000,
+    reset: false
+}); 
